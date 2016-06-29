dc9751dd79dcf2816a6772e6b9366e0c851c5a07
Jan de Cooman <jan.decooman@neofonie.de>
2016-06-29 11:44:22 +0200

    upgrade to 3.6.3

--- src/main/java/de/neofonie/hazelcast/consul/ConsulDiscovery.java
+++ src/main/java/de/neofonie/hazelcast/consul/ConsulDiscovery.java
@@ -31,6 +31,8 @@ import com.orbitz.consul.CatalogClient;
 import com.orbitz.consul.Consul;
 import com.orbitz.consul.model.ConsulResponse;
 import com.orbitz.consul.model.catalog.CatalogService;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
@@ -47,7 +49,7 @@ public class ConsulDiscovery implements DiscoveryStrategy {
 
     private static final Logger LOG = Logger.getLogger(ConsulDiscovery.class.getName());
 
-    private CatalogClient agentClient;
+    private CatalogClient catalogClient;
 
     final private String consulHost;
 
@@ -73,38 +75,63 @@ public class ConsulDiscovery implements DiscoveryStrategy {
 
         LOG.log(Level.INFO, "Starting Consul Discovery");
 
-        this.agentClient = Consul.newClient(consulHost, consulPort).
-                catalogClient();
     }
 
+    /**
+     * Construct the client to connect to Consul.
+     * 
+     * @return CatalogClient
+     * @throws MalformedURLException 
+     */
+    private synchronized CatalogClient getClient() throws MalformedURLException {
+        if (this.catalogClient == null) {
+            this.catalogClient = Consul.builder().
+                    withUrl(new URL("http", consulHost, consulPort, "")).
+                    build().
+                    catalogClient();
+        }
+        return this.catalogClient;
+    }
+
+    /**
+     * Look in consul for nodes with a given name. Exceptions are not rethrown
+     * to prevent HazelCast of shutting down. This method returns an empty
+     * list in case of error. When for example, consul returns to a normal state
+     * a list of nodes is returned. HazelCast will try to connect to this
+     * nodes.
+     * 
+     * @return A Collection of Nodes.
+     */
     @Override
     public Collection<DiscoveryNode> discoverNodes() {
 
         Collection<DiscoveryNode> list = new LinkedList<>();
+        try {
 
-        if (this.agentClient != null) {
-            try {
+            ConsulResponse<List<CatalogService>> service
+                    = this.getClient().getService(serviceName);
 
-                ConsulResponse<List<CatalogService>> service = this.agentClient.getService(serviceName);
-
-                for (CatalogService s : service.getResponse()) {
-                    if (LOG.isLoggable(Level.FINEST)) {
-                        LOG.log(Level.FINEST, "Found service at: {0}", s.getAddress());
-                    }
-                    Address address = new Address(s.getAddress(), s.getServicePort());
-                    list.add(new SimpleDiscoveryNode(address));
+            for (CatalogService s : service.getResponse()) {
+                if (LOG.isLoggable(Level.FINEST)) {
+                    LOG.log(Level.FINEST, "Found service at: {0}", s.getAddress());
                 }
-
-                LOG.log(Level.INFO, "Hazelcast found ''{0}'' instance(s) of ''{1}'' ", new Object[]{list.size(), serviceName});
-
-                return list;
-            } catch (Exception e) {
-                // The exception is no longer rethrown. When the exception
-                // arrives in Hazelcast-core, it will stop working totally.
-                // An empty list is delivered as default.
-                LOG.severe(e.getMessage());                
+                Address address = new Address(s.getAddress(), s.getServicePort());
+                list.add(new SimpleDiscoveryNode(address));
             }
+
+            LOG.log(Level.INFO, "Hazelcast found ''{0}'' instance(s) of ''{1}'' ", new Object[]{list.size(), serviceName});
+
+            return list;
+            
+        } catch (Exception e) {
+            // The exception is no longer rethrown. When the exception
+            // arrives in Hazelcast-core, it will stop working totally.
+            // An empty list is delivered as default. This method will be
+            // invoked again after a certain time. When consul is available,
+            // it will answer, otherwise we just retry.
+            LOG.severe(e.getMessage());
         }
+
         return list;
     }
 
